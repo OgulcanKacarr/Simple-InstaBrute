c7f76586c814e4d0c00f9024066f998d8ad3ac84
OgulcanKacarr <63792003+OgulcanKacarr@users.noreply.github.com>
2020-12-15 11:25:35 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from colorama import Fore, Back, Style
+from termcolor import colored, cprint
+import colorama
+import time
+import os
+
+
+colorama.init()
+print(Fore.RED)
+os.system("pip install selenium")
+print(Fore.GREEN)
+os.system("pip install colorama ")
+print(Fore.RED)
+os.system("pip install termcolor ")
+print(Fore.GREEN)
+os.system("pip install wget")
